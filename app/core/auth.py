8a1dc90025d2adcbf241f69b59339cce4565fb23
Sheggam Harshith <harshit675@gmail.com>
2022-07-22 14:45:47 +0530

    Update auth.py

--- app/core/auth.py
+++ app/core/auth.py
@@ -97,12 +97,12 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserInDB:
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
+#     try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    username = payload.get("sub")
+    if username is None:
+        raise credentials_exception
+    token_data = TokenData(username=username)
 #     except PyJWTError:
 #         raise credentials_exception
     user = get_user(fake_users_db, username=token_data.username)
